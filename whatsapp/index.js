a4684620d8717cbfb449b8be7aa8ccece3504482
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-09-30 10:12:01 +0530

    testing multidevice beta

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-// const qrcode = require('qrcode-terminal');
+// const fs = require('fs');
+const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 var app = require("express")();
 var http = require('http').Server(app);
@@ -20,36 +20,39 @@ http.listen(3000, function(){
     console.log('listening...');
 });
 
-// Path where the session data will be stored
-const SESSION_FILE_PATH = './session.json';
+// // Path where the session data will be stored
+// const SESSION_FILE_PATH = './session.json';
 
-// Load the session data if it has been previously saved
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
+// // Load the session data if it has been previously saved
+// let sessionCfg;
+// if(fs.existsSync(SESSION_FILE_PATH)) {
+//     sessionCfg = require(SESSION_FILE_PATH);
+// }
 
 // Use the saved values
 const client = new Client({
-    session: sessionData
+    // puppeteer: { headless: true }
+    // clientId: 'remote'
 });
 
 // Save session values to the file upon successful auth
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+client.on('authenticated', () => {
+    console.log('youre in');
+    // sessionData = session;
+    // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    //     if (err) {
+    //         console.error(err);
+    //     }
+    // });
 });
 
-// client.on('qr', qr => {
-//     qrcode.generate(qr, {small: true});
-// });
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
 client.on('ready', () => {
     console.log('Client is ready!');
+    client.sendMessage("918592988798@c.us", "Hello!");
 });
 
 client.initialize();

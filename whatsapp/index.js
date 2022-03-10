d3d6d72c799df5b09bea1ec05049becdecaeccf7
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2022-03-10 08:10:10 +0530

    wweb.js update

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -1,5 +1,5 @@
 const qrcode = require('qrcode-terminal');
-const { Client, MessageMedia} = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia} = require('whatsapp-web.js');
 var app = require("express")();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
@@ -29,30 +29,17 @@ http.listen(3000, function () {
     console.log('listening...');
 });
 
-// // Path where the session data will be stored
-// const SESSION_FILE_PATH = './session.json';
-
-// // Load the session data if it has been previously saved
-// let sessionCfg;
-// if(fs.existsSync(SESSION_FILE_PATH)) {
-//     sessionCfg = require(SESSION_FILE_PATH);
-// }
 
 // Use the saved values
 const client = new Client({
     // puppeteer: { headless: false }
     // clientId: 'remote'
+    authStrategy: new LocalAuth()
 });
 
 // Save session values to the file upon successful auth
 client.on('authenticated', () => {
     console.log('youre in');
-    // sessionData = session;
-    // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-    //     if (err) {
-    //         console.error(err);
-    //     }
-    // });
 });
 
 client.on('qr', qr => {
